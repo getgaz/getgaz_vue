81fc06be5e8ea266c7ad1b8076e9e5589926da3d
zero4994 <cavc94@gmail.com>
2019-02-07 15:22:23 +0900

    Added mock information to display markers on a map

--- src/store.js
+++ src/store.js
@@ -1,34 +1,51 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import axios from "axios";
+//import axios from "axios";
+const mockLocations = require("../data/mock.json");
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    locations: [],
+    locations: []
   },
   mutations: {
     setLocations(state, locations) {
       state.locations = locations;
-    },
+    }
   },
   actions: {
+    // async loadMarkers({ commit }) {
+    //   try {
+    //     const { data: locations } = await axios.get("/api/locations"); // ES6 destructuring & aliasing
+    //     const markers = locations.map((location) => ({
+    //       position: {
+    //         lat: location.latitude,
+    //         lng: location.longitude,
+    //       },
+    //       key: location.name,
+    //       defaultAnimation: 2,
+    //     }));
+    //     commit("setLocations", markers);
+    //   } catch (err) {
+    //     console.error(err);
+    //   }
+    // },
     async loadMarkers({ commit }) {
       try {
-        const { data: locations } = await axios.get("/api/locations"); // ES6 destructuring & aliasing
-        const markers = locations.map((location) => ({
+        const locations = mockLocations;
+        const markers = locations.map(location => ({
           position: {
-            lat: location.latitude,
-            lng: location.longitude,
+            lat: location.Site.Latitude,
+            lng: location.Site.Longitude
           },
-          key: location.name,
-          defaultAnimation: 2,
+          key: location.Name,
+          defaultAnimation: 2
         }));
         commit("setLocations", markers);
       } catch (err) {
         console.error(err);
       }
-    },
-  },
+    }
+  }
 });
