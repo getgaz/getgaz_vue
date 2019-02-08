edfa89488d7acd8b79b0b87661bc66341ffa5c8a
zero4994 <cavc94@gmail.com>
2019-02-08 15:35:55 +0900

    Clened component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,5 @@
 <template>
   <nav class="navbar navbar-dark footer mt-5 bg-dark">
-    <!-- <div class="nav-item">
-      <div class="row">
-        <div class="col"></div>
-        <div class="col p-2">
-          
-        </div>
-        <div class="col"></div>
-      </div>
-    </div>-->
     <div class="container">
       <small class="mx-auto footer_font-color">Made with ❤️ by the getgaz team</small>
     </div>
