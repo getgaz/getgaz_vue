64acb86c50755de0885b6077a251009f930e75a6
zero4994 <cavc94@gmail.com>
2019-02-08 15:30:37 +0900

    Improved styling

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,17 @@
 <template>
-  <nav class="navbar navbar-dark footer mt-5">
-    <h5 class="navbar-brand">Made with ❤️ by us</h5>
+  <nav class="navbar navbar-dark footer mt-5 bg-dark">
+    <!-- <div class="nav-item">
+      <div class="row">
+        <div class="col"></div>
+        <div class="col p-2">
+          
+        </div>
+        <div class="col"></div>
+      </div>
+    </div>-->
+    <div class="container">
+      <small class="mx-auto footer_font-color">Made with ❤️ by the getgaz team</small>
+    </div>
   </nav>
 </template>
 
@@ -10,7 +21,8 @@ export default {};
 
 
 <style scoped>
-.footer {
-  background-color: #72a0ea;
+.footer_font-color {
+  font-size: 1.2rem;
+  color: #939393;
 }
 </style>
