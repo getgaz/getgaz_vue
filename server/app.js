3fadac97ea52dca691816214a7ecda3c8e97c20f
Keisuke Mori <morino.ku.masan1990@gmail.com>
2019-02-08 15:51:35 +0900

    WIP server/app.js

--- server/app.js
+++ server/app.js
@@ -17,8 +17,28 @@ app.use(express.static(path.resolve(__dirname, "..", "dist")));
 
 app.get("/api/locations", async (req, res) => {
   try {
-    const locations = await db.select().table("locations");
-    res.json(locations);
+    const results = [];
+    const locations = await db("locations").select();
+    for (const location of locations) {
+      const siteId = location.site_id;
+      const amenities = await db("location_amenity")
+        .innerJoin("amenities", "location_amenity.amenity_id", "amenities.id")
+        .where({
+          location_id: siteId
+        })
+        .select("id", "display_name");
+      const result = {
+        site_id: location.site_id,
+        name: location.name,
+        latitude: location.latitude,
+        longitude: location.longitude,
+        address: location.address,
+        amenities: amenities
+      };
+      results.push(result);
+    }
+
+    res.json(results);
   } catch (err) {
     console.error("Error loading locations!", err);
     res.sendStatus(500);
