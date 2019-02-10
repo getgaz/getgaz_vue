8cd51907b950beb9e97433c3ee3fc9d01dd5abc6
zero4994 <cavc94@gmail.com>
2019-02-10 14:49:35 +0900

    Fix styling for the card components

--- src/components/AllLocations.vue
+++ src/components/AllLocations.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
     <div class="card mb-3" v-for="(location, index) in this.$store.state.locations" :key="index">
-      <div class="card-header">{{location.key}}</div>
+      <div class="card-header">
+        <h3>{{location.key}}</h3>
+      </div>
       <div class="card-body bg-dark">
-        <h5 class="card-subtitle mb-2 text-muted">Address</h5>
-        <label class="ml-2">
+        <h5 class="card-subtitle mb-2 text-white">Address</h5>
+        <label class="ml-2 text-white">
           {{
           `${location.address}`
           }}
         </label>
-        <h5 class="card-subtitle mt-2 text-muted mb-2">Amenities</h5>
+        <h5 class="card-subtitle mt-2 text-white mb-2">Amenities</h5>
         <ul class="list-group">
           <li
             v-for="(amenity, ind) in location.amenities"
